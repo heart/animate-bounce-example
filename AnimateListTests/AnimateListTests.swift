83c53a71ae33aedffa3bb92bb3f77b56af315baf
heart <narongrit@3dsinteractive.com>
2020-07-05 13:20:22 +0700

    Initial Commit

---
+++ AnimateListTests/AnimateListTests.swift
@@ -0,0 +1,34 @@
+//
+//  AnimateListTests.swift
+//  AnimateListTests
+//
+//  Created by narongrit kanhanoi on 5/7/2563 BE.
+//  Copyright © 2563 narongrit kanhanoi. All rights reserved.
+//
+
+import XCTest
+@testable import AnimateList
+
+class AnimateListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
